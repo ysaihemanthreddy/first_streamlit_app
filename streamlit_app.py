6d7ecf644834d135afb07e4d7a12f2a256de020f
ysaihemanthreddy <104008607+ysaihemanthreddy@users.noreply.github.com>
2022-08-04 13:10:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLERROR
+from urllib.error import URLError
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
